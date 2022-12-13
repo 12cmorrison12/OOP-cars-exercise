61a5877a34c119ce47c800f20e9668c3b81fdf31
unknown <12cmorrison12@gmail.com>
2022-12-12 19:42:28 -0700

    finished part one

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Vehichle {
+ constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+ }   
+
+ honk() {
+    return 'HONK!';
+ }
+
+ toString() {
+    return `Your vehichle is a ${this.make} ${this.model} from ${this.year}`;
+ }
+}
+
+class Car {
     
 }
