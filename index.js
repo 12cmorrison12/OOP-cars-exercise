1020ae3d86bcd1805e24b846597f53a8c390d582
unknown <12cmorrison12@gmail.com>
2022-12-12 20:13:37 -0700

    finished part 3 and 4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Vehichle {
+class Vehicle {
     constructor(make, model, year) {
         this.make = make;
         this.model = model;
@@ -14,10 +14,38 @@ class Vehichle {
     }
 }
 
-class Car extends Vehichle {
+class Car extends Vehicle {
     super(make, model, year);
 
     constructor(numWheels) {
-        this.numWheels = 4;
+        numWheels = 4;
     }  
+}
+
+class Motorcycle extends Vehicle {
+    super(make, model, year);
+    constructor(numWheels) {
+        numWheels = 2;
+    }
+
+    revEngine() {
+        return 'VROOM!!'
+    };
+}
+
+class Garage {
+    constructor(vehicles) {
+        capacity = 8;
+        vehicles = [];
+    }
+
+    add() {
+        for(let vehicles of Car) {
+            if(vehicles != Car || Motorcycle) {
+                throw new Error('Only vehicles are allowed in here!');
+            }
+
+            new vehicles = vehicles.push(Car, Motorcycle);
+        }
+    }
 }
